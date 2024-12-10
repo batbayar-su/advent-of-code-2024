820a71f4a3d0836c11750ceee8f8ae3c1ab84d09
Batbayar Sukhbaatar <batbayar.sukhbaatar@sap.com>
2024-12-10 22:50:55 +0100

    day 1 in deno

---
+++ days/1/partOne.ts
@@ -0,0 +1,20 @@
+const input = await Deno.readTextFile(`${import.meta.dirname}/input.txt`);
+const lines = input.trim().split("\n");
+const left: number[] = [];
+const right: number[] = [];
+
+for (const line of lines) {
+  const [one, two] = line.split("   ");
+  left.push(parseInt(one));
+  right.push(parseInt(two));
+}
+
+left.sort();
+right.sort();
+
+let result = 0;
+for (let i = 0; i < left.length; i++) {
+  result += Math.abs(left[i] - right[i]);
+}
+
+console.log(result);
---
+++ days/1/partTwo.ts
@@ -0,0 +1,32 @@
+const input = await Deno.readTextFile(`${import.meta.dirname}/input.txt`);
+const lines = input.trim().split("\n");
+const left: number[] = [];
+const right: number[] = [];
+
+for (const line of lines) {
+  const [one, two] = line.split("   ");
+  left.push(parseInt(one));
+  right.push(parseInt(two));
+}
+
+const cache: Record<number, number> = {};
+
+function countOccurances(find: number, list: number[]) {
+  const found = list.filter((target) => find === target);
+  return found.length;
+}
+
+let result = 0;
+for (let i = 0; i < left.length; i++) {
+  const seed = left[i];
+
+  let acc = cache[seed];
+  if (!acc) {
+    acc = countOccurances(seed, right);
+    cache[seed] = acc;
+  }
+
+  result += seed * acc;
+}
+
+console.log(result);
