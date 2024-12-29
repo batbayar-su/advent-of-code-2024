7330964454bf89d00820466cd4f677ebf0743fac
Batbayar Sukhbaatar <batbayar.sukhbaatar@sap.com>
2024-12-29 09:33:49 +0100

    day 7 part one

---
+++ days/7/partOne.ts
@@ -0,0 +1,39 @@
+type Equation = [number, number[]];
+
+function testEquation([testValue, numbers]: Equation, total: number, index: number): boolean {
+  if (index === numbers.length) {
+    return total === testValue;
+  }
+
+  const plusR = testEquation([testValue, numbers], total + numbers[index], index + 1);
+  if (plusR) {
+    return true;
+  }
+  const multR = testEquation([testValue, numbers], total * numbers[index], index + 1);
+  return multR;
+}
+
+function sumOfCorrectEquations(equations: Equation[]): number {
+  return equations.reduce((sum, equation) => {
+    const result = testEquation(equation, 0, 0);
+    return sum += result ? equation[0] : 0;
+  }, 0);
+}
+
+function parseInput(input: string): Equation[] {
+  return input.trim().split('\n').map((line) => {
+    const [testValue, numbersRaw] = line.split(': ');
+    const numbers = numbersRaw.split(' ').map((value) => parseInt(value));
+    return [parseInt(testValue), numbers];
+  });
+}
+
+async function main() {
+  const input = await Deno.readTextFile(`${import.meta.dirname}/input.txt`);
+
+  const equations = parseInput(input);
+
+  console.log(sumOfCorrectEquations(equations));
+}
+
+await main();
